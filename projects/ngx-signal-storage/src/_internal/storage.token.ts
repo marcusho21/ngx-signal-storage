6ac0acfa42f26ad3db271ec7b9eb756ac2665989
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2023-10-28 14:14:25 -0400

    feat: setup ineternal localstorage injection token

---
+++ projects/ngx-signal-storage/src/_internal/storage.token.ts
@@ -0,0 +1,13 @@
+import { DOCUMENT } from '@angular/common';
+import { InjectionToken, inject } from '@angular/core';
+
+export const STORAGE = new InjectionToken('LocalStorage', {
+  providedIn: 'root',
+  factory: () => {
+    const { defaultView } = inject(DOCUMENT);
+    if (!defaultView?.localStorage)
+      throw new Error('LocalStorage is not supported');
+
+    return defaultView.localStorage;
+  },
+});
